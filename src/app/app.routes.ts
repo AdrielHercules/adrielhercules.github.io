ab94a87e3d84e0588a5dee5e8012c4d814eded65
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-09-15 20:42:09 +0200

    Set the default route to wip-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { WipPage } from './core/wip/pages/wip-page/wip-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: WipPage,
+    title: 'Work in progress',
+  },
+];
