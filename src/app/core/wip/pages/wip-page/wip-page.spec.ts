d1f16aa562706f6f3e8c35ba112957f84f001531
AdrielHercules <109773902+AdrielHercules@users.noreply.github.com>
2025-09-15 20:41:33 +0200

    Add wip page

---
+++ src/app/core/wip/pages/wip-page/wip-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WipPage } from './wip-page';
+
+describe('WipPage', () => {
+  let component: WipPage;
+  let fixture: ComponentFixture<WipPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [WipPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WipPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/core/wip/pages/wip-page/wip-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wip-page',
+  imports: [],
+  templateUrl: './wip-page.html',
+  styleUrl: './wip-page.css'
+})
+export class WipPage {
+
+}
